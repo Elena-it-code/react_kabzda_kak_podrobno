6d7a794998ff47f13b9bec0077e652d3f00dcf2d
ElenaKuklina <cuklina.lena@gmail.com>
2023-11-28 01:29:53 +0300

    Add component Rating.tsx (Example with star)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {OnOff} from "./On/OnOff";
 import {UnControlAccordion} from "./UnControlAccordion/UnControlAccordion";
 import {UnControlRating} from "./UnControlRating/UnControlRating";
+import {Rating, RatingValueType} from "./Rating/Rating";
 
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className="App">
-           <OnOff />
+            <OnOff/>
             <UnControlAccordion titleValue={'Menu'}/>
             <UnControlAccordion titleValue={'Users'}/>
-            <UnControlRating />
+            <UnControlRating/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
 
-           {/*<OnOff on={false}/>
+            {/*<OnOff on={false}/>
            <OnOff on={true}/>*/}
         </div>
     );

0ec7543b04f674735565443d7e32425c79c46d7e
ElenaKuklina <cuklina.lena@gmail.com>
2023-11-27 23:31:38 +0300

    Update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {OnOff} from "./On/OnOff";
+import {UnControlAccordion} from "./UnControlAccordion/UnControlAccordion";
+import {UnControlRating} from "./UnControlRating/UnControlRating";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+           <OnOff />
+            <UnControlAccordion titleValue={'Menu'}/>
+            <UnControlAccordion titleValue={'Users'}/>
+            <UnControlRating />
+
+           {/*<OnOff on={false}/>
+           <OnOff on={true}/>*/}
+        </div>
+    );
 }
 
 export default App;

fee9a39a7d99189ae5bf002a0a914e5bda6610a3
ElenaKuklina <cuklina.lena@gmail.com>
2023-11-29 20:33:45 +0300

    Renamed component OnOf in UnControlOnOf

---
+++ src/On/UnControlOnOff.tsx
@@ -0,0 +1,47 @@
+import {useState} from "react";
+
+
+type PropsType = {
+    //on: boolean
+}
+
+export function UnControlOnOff (props:PropsType) {
+   /*let on = false;*/
+   let[on, setOn] = useState(false); // hook
+    const onStyle ={
+        width: '50px',
+        height: '30px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '10px',
+        backgroundColor: on ? 'green': 'white'
+    };
+    const offStyle={
+        width: '50px',
+        height: '30px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '10px',
+        padding: '10px',
+        backgroundColor: on ? 'white' : 'red'
+    };
+    const indicatorStyle={
+        width: '30px',
+        height: '30px',
+        borderRadius: '15px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginBottom: '-15px',
+        marginLeft: '20px',
+        backgroundColor: on ? 'green' : 'red'
+    };
+
+
+    return (
+        <div>
+            <div style={onStyle} onClick={()=>{setOn(true)}}>On</div>
+            <div style={offStyle} onClick={()=>{setOn(false)}}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}

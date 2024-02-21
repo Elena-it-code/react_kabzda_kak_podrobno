19a2420a15270b32a2b5ccfb3eee3b0ac5b042a9
ElenaKuklina <cuklina.lena@gmail.com>
2024-02-21 17:14:46 +0300

    16_Lesson: unit tests for reducer

--- src/components/UnControlAccordion/UnControlAccordion.tsx
+++ src/components/UnControlAccordion/UnControlAccordion.tsx
@@ -1,5 +1,5 @@
 import {useReducer} from "react";
-import {reducer, TOGGLE_CONSTANT} from "./reducer";
+import {reducer, TOGGLE_COLLAPSED} from "./reducer";
 
 export type AccordionPropsType = {
     titleValue: string
@@ -9,18 +9,18 @@ export type AccordionPropsType = {
 export function UnControlAccordion (props: AccordionPropsType) {
 
     //let[collapsed, setCollapsed]=useState(true)
-    let[state, dispatch]=useReducer(reducer, { collapsed: false})        // говорим на тебе reduser пользуйся им, и начальное значение. т.к. у нас state boolean значение, то поэтому говорим, что false
+    let[state, dispatch]=useReducer(reducer, { collapsed: false})        // говорим на тебе reducer пользуйся им, и начальное значение. т.к. у нас state boolean значение, то поэтому говорим, что false
 
     return (
         <div>
             {/*<AccordionTitle title={props.titleValue} onClick={()=>{setCollapsed(!collapsed)}}/>*/}
 
-            {/*при нажатии на кнопку AccordionTitle title мы dispath(им) инструкцию type: "TOGGLE-COLLAPSED" в React,
-            dispath() - это f, которая вылезла из useReducer(а), с помощбю которой React позволяет нам отправлять в него вот эти вот команды action(экшины),
-            чтобы можно было преобразовать state. Стартовое значение котрого передается тут: useReducer(reduсer, стартовое значение: false)
-            с помощью этого reduсer(а) по этим правилам (action.type === "TOGGLE-COLLAPSED")
+            {/*при нажатии на кнопку AccordionTitle title мы dispatch(им) инструкцию type: "TOGGLE-COLLAPSED" в React,
+            dispatch() - это f, которая вылезла из useReducer(а), с помощбю которой React позволяет нам отправлять в него вот эти вот команды action(экшины),
+            чтобы можно было преобразовать state. Стартовое значение котрого передается тут: useReducer(reducer, стартовое значение: false)
+            с помощью этого reducer(а) по этим правилам (action.type === "TOGGLE-COLLAPSED")
             */}
-            <AccordionTitle title={props.titleValue} onClick={()=>{dispatch({type: TOGGLE_CONSTANT })}}/>       {/*мы должны в dispath отправить action. action - это объект.сщздаем объект{},  у этого объекта ОБЯЗАТЕЛЬНО должен быть type. Инструкция что нужно сделать "БОЛЬШИШИМИ-БУКВАМИ" */}
+            <AccordionTitle title={props.titleValue} onClick={()=>{dispatch({type: TOGGLE_COLLAPSED })}}/>       {/*мы должны в dispatch отправить action. action - это объект.сщздаем объект{},  у этого объекта ОБЯЗАТЕЛЬНО должен быть type. Инструкция что нужно сделать "БОЛЬШИШИМИ-БУКВАМИ" */}
             {/*<button onClick={()=>{setCollapsed(!collapsed)}}>TOGGLE</button>*/}
             { !state.collapsed && <AccordionBody/>}
         </div>
--- src/components/UnControlAccordion/reducer.tsx
+++ src/components/UnControlAccordion/reducer.tsx
@@ -2,21 +2,21 @@ type ActionType = {
     type: string     // обязательно есть это свойство. надпись, что именно нужно сделать н/р: "REMOVE-TASK", "ADD-TASK" и т.д.
 }
 
-type StateType = {
+export type StateType = {
     collapsed: boolean
 }
-export const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED"     // использование CONSTANt не даст нам ошибиться при написании свойств, опечататься случайно. Далее используем уже эти const
+export const TOGGLE_COLLAPSED = "TOGGLE-COLLAPSED"     // использование CONSTANt не даст нам ошибиться при написании свойств, опечататься случайно. Далее используем уже эти const
 export const reducer = (state: StateType, action: ActionType): StateType => {
     // самая топовая запись именно так:
     switch (action.type) {
-        case TOGGLE_CONSTANT:
+        case TOGGLE_COLLAPSED:
             /*const stateCopy = {...state}                  // можно итак созадть копию, а затем присвоить collapsed новое значение
             stateCopy.collapsed = !state.collapsed*/
             return  {...state, collapsed: !state.collapsed}    // создали копию объекта в одну строку, сразу переприсвоили значениие collapsed. Возвращаем объект, копируем все старое, что в нем было и меняем значение у свойства collapsed на противоположное, которое сидело в старом state -- !state.collapsed--
         default:
             //return state                                   // action который Reduсer не ждет нет смысла dispath(тить)
             throw new Error("Bad action type")      // как один из вариантов мы можем сгенерировать новую ошибку в default:
-        // if (action.type === TOGGLE_CONSTANT) {               // если к нам пришел объект action и у него в этой инструкции сидит тип "TOGGLE-COLLAPSED", т.е. "совпало ключ и замок"
+        // if (action.type === TOGGLE_COLLAPSED) {               // если к нам пришел объект action и у него в этой инструкции сидит тип "TOGGLE-COLLAPSED", т.е. "совпало ключ и замок"
         //   return !state                                    // то мы тогда вернем противоподложное значение стэйта !state
     }
 

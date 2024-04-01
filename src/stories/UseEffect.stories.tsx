2ac6ca961dce2dff4e2e3f083e9837e0b6ca9fe9
ElenaKuklina <cuklina.lena@gmail.com>
2024-04-01 17:09:36 +0300

    21- useEffect основы

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from "react";
+
+
+export default {
+    title: 'UseEffect demo',
+}
+
+export const SimpleExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1); // [4847633949, function(newValue){}]
+
+    console.log("SimpleExample")
+
+    useEffect(() => {
+        // изменение заголовка у документа
+        document.title = counter.toString()
+        console.log("useEffect every render ")
+    }); // сработает каждый раз при рендере компоненты
+
+    useEffect(() => {
+        // изменение заголовка у документа
+        document.title = counter.toString()
+        console.log("useEffect only first render (componentDidMount)")
+
+    }, []); // сработает один раз, в момент, когда компонента только вмонтировалась, в первый рендер
+
+    useEffect(() => {
+        // изменение заголовка у документа
+        document.title = counter.toString()
+        console.log("useEffect first render and every counter changed")
+
+    }, [counter]); // сработает при первом рендере, и при изменении зависимости
+
+    return <>
+        Hello! {counter}
+        <button onClick={()=> {setFake(fake+1)}}>fake +</button>
+        <button onClick={()=> {setCounter(counter+1)}}>counter +</button>
+    </>
+}
+
+// В useEffect() мы переадли callback f-ю ()=>{} , по сути эта callback функция и содержит
+// какую-то лигику, которую можно назвать Effect(ом) или назвать sideEffect(ом)
+
+// ЧТО ТАКОЕ sideEffect в React:
+// api.getUser().then('') запрос на сервер
+// setInterval - установка интервалов
+// indexDB - обращение к базе данных
+// document.getElement.Id - обращение к документу, нопрямую к DOM элементам, редко, но бывает
+// document.title = " User " - изменение заголовка у документа
+// и т.д. Т.е. любая асинхронщина. Делаем что-то "грязное" за рамками компоненты
+
+// При каждой перерисовки компоненты, useEffect() будет отрабатывать
+// ВАЖНЫЙ МОМЕНТ: сначала происходит отрисовка контента, потом только запускается useEffect()
+
+// если мы в зависимость вообще не передали ничего, не пустой массив, а ВООБЩЕ НИЧЕГО useEffect(()=>{}),
+// то мы каждый раз будем запускаться при рендере компоненты
+// Если передали конкретную зависмость в useEffect( ()=>{}, deps: [counter]), то UseEffect() срабатывает первый раз,
+// а потом при смене этой зависимости, как она изменилась, то и запустится наш UseEffect()
+
+// Если мы передали пустой массив в зависмость в useEffect( ()=>{}, deps: []), то UseEffect() срабатывает один раз,
+// при монтировании компоненты, её зарождении. При изменении компоненты он срабатывать больше не будет
